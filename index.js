ba13b0b4b52833abdb4485bb99ed21cd5b48139e
Iuliia Tokarevskaia <iuliia.tokarevskaia@socialbakers.com>
2021-09-29 09:50:13 +0200

    Buildfix

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const ws = require('ws');
+const express = require('express');
 
 const PORT = process.env.PORT || 3000;
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .use((req, res) => res.sendFile('/index.html', { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new ws.Server({ server });
